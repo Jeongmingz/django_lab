a499cf38288a7314c3b78f8abacf29845ed27c5f
Jeongmingz <joungminyt@gmail.com>
2025-03-19 10:25:20 +0900

    Feat: Django REST Framework Spectacular 구현

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import SpectacularRedocView, SpectacularSwaggerView, SpectacularAPIView, \
+    SpectacularJSONAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+#     Django REST Framework Spectacular
+    path('docs/json/', SpectacularJSONAPIView.as_view(), name='schema-json'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/user', SpectacularAPIView.as_view(), name='user_schema'),
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(), name = 'swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(), name='redoc')
 ]
